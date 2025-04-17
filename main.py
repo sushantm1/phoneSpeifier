d5fc0cdc946c6830ae43bc6417586a7d62eb2b46
sushantm1 <sushantm1100202@gmail.com>
2025-04-17 23:40:54 +0530

    update

--- main.py
+++ main.py
@@ -13,18 +13,37 @@ brand_names = data['brand_name'].drop_duplicates().reset_index(drop=True)
 print("Available brands:\n", brand_names)
 
 # Input first brand from user
-first_company_name = input("Enter the first company name: ").lower()
+first_phone_name = input("Enter the first company name: ").lower()
 
 # Filter models by brand
-if first_company_name in brand_names.values:
-    print(data)
-    model_data = data[data['brand_name'] == first_company_name].reset_index(drop=True)
-    print("Available models from", first_company_name, ":\n", model_data['model'])
+if first_phone_name in brand_names.values:
+    # print(data)
+    phone1_model_data = data[data['brand_name'] == first_phone_name].reset_index(drop=True)
+    print("Available models from", first_phone_name, ":\n", phone1_model_data['model'])
 else:
     print("Brand not found. Please check your input.")
 
-index_of_model = int(input("Enter the index of the model : "))
-phone_sepcs=data.loc[(data['brand_name']==first_company_name) & (data['model'] == model_data['model'][index_of_model])].reset_index(drop=True)
+index_of_model1 = int(input("Enter the index of the model 1 : "))
 
-for i in phone_sepcs:
-    print(i, " : ", phone_sepcs[i][0])
+sec_phone_name = input("Enter the first company name: ").lower()
+
+if sec_phone_name in brand_names.values:
+    # print(data)
+    phone2_model_data = data[data['brand_name'] == sec_phone_name].reset_index(drop=True)
+    print("Available models from", sec_phone_name, ":\n", phone2_model_data['model'])
+else:
+    print("Brand not found. Please check your input.")
+
+
+index_of_model2 = int(input("Enter the index of the model 2 : "))
+
+phone_sepcs1=data.loc[(data['brand_name']==first_phone_name) & (data['model'] == phone1_model_data['model'][index_of_model1])].reset_index(drop=True)
+phone_sepcs2=data.loc[(data['brand_name']==sec_phone_name) & (data['model'] == phone2_model_data['model'][index_of_model2])].reset_index(drop=True)
+
+for i in phone_sepcs1:
+    print(i, " : ", phone_sepcs1[i][0])
+
+print("\n\n")
+for i in phone_sepcs2:
+    print(i, " : ", phone_sepcs2[i][0])
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,3 @@
-if first_company_name in brand_names.values:
-#     model_data = data[data['brand_name'] == first_company_name].reset_index(drop=True)
-#     print("Available models from", first_company_name, ":\n", model_data['model'])
-# else:
-#     print("Brand not found. Please check your input.")
+in phone_sepcs1.columns:  
 
+#     print(i, " : 
